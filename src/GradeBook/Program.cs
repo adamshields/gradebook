6a4c985e57e980b6edad15e4ef50cdbbdf85ee48
Adam Shields <adamenote@gmail.com>
2020-09-08 12:05:06 -0400

    Computed Grades - Computing Statistics

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -18,12 +18,17 @@ namespace GradeBook
 
             var result = 0.0;
             var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
             foreach (var number in grades)
             {
+                lowGrade = Math.Min(number, lowGrade);
                 highGrade = Math.Max(number, highGrade);
                 result += number;
             }
             result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
             Console.WriteLine($"The average grade is {result:N1}");
         }
     }
